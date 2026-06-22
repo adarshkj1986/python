11c6f1c8841ef2a4d55049336f5a8a8a53b9d90d
adarshkj1986 <adarshkj1986@gmail.com>
2026-06-22 13:52:00 +0530

    23 uploaded

---
+++ 23/49.py
@@ -0,0 +1,7 @@
+def string(n):
+    count=0
+    for char in n:
+        count+=1
+    return count
+n="hello"
+print("len of string is",string(n))
---
+++ 23/50.py
@@ -0,0 +1,5 @@
+text="everyone"
+reverse=""
+for char in text:
+    reverse=char+reverse
+print("reverse is:",reverse)
