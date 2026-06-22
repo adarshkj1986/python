9e04ea3fe9bcee801fe555032d0fbdaa0ea49ef3
adarshkj1986 <adarshkj1986@gmail.com>
2026-06-22 13:57:01 +0530

    25 uploaded

---
+++ 25/53.py
@@ -0,0 +1,5 @@
+text=input("enter the string:")
+if text==text[::-1]:
+    print("this is palindrome")
+else:
+    print("not palindrome")
---
+++ 25/54.py
@@ -0,0 +1,4 @@
+sentence=input("enter the sentence:")
+words=sentence.split()
+count=len(words)
+print("total words are:",count)
