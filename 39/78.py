0524ed55ec7636e71863224821bd93c9fdb23f20
adarshkj1986 <adarshkj1986@gmail.com>
2026-06-27 14:43:01 +0530

    39

---
+++ 39/78.py
@@ -0,0 +1,9 @@
+def rotate_array(a,k):
+    n=len(a)
+    if n==0:
+        return a
+    k=k%10
+    return a[k:]+a[:k]
+a=[6,7,8,9,10]
+k=2
+print("the rotated array to the left direction is:",rotate_array(a,k))
