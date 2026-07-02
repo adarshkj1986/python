9e73e2d2d05cdfa28cab93ac9fd1204ba9d10fa4
adarshkj1986 <adarshkj1986@gmail.com>
2026-07-02 18:05:36 +0530

    56

---
+++ 56/99.py
@@ -0,0 +1,11 @@
+def selection_sort(a):
+    n=len(a)
+    for i in range(n):
+        min_e=i
+        for j in range(i+1,n):
+            if a[j]<a[min_e]:
+                min_e=j
+                a[i],a[min_e]=a[min_e],a[i]
+        return a
+a=[5,4,2,6,1]
+print("array is:",selection_sort(a))
