6f42cd322552d1e8e0ed3fc0cd1abf1124493296
adarshkj1986 <adarshkj1986@gmail.com>
2026-07-04 19:12:02 +0530

    62

---
+++ 62/106.py
@@ -0,0 +1,17 @@
+n=int(input("enter the number:"))
+count=0
+temp=n
+n1=n
+while(temp>0):
+    temp=temp//10
+    count+=1
+sum=0
+n=n1
+while(n>0):
+    r=n%10
+    sum=sum+pow(r,count)
+    n=n//10
+if(n1==sum):
+    print("this is a armstrong number")
+else:
+    print("this is not a armstrong number:")
