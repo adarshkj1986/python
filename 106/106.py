f7856987f32de6c80c4b8c287daacc9303f34ab5
adarshkj1986 <adarshkj1986@gmail.com>
2026-08-07 19:04:46 +0530

    106

---
+++ 106/106.py
@@ -0,0 +1,11 @@
+#check number is positive,negative and zero
+print("Adarsh Kumar Jha")
+a=int(input("enter the number"))
+if(a>0):
+    print("this is a positive number")
+elif(a<0):
+    print("this is a negative number")
+elif(a==0):
+    print("a is zero")
+else:
+    print("error")
